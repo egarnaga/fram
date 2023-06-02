515d16f4f02ee6e84f694e575da36c2e09b4ee03
Evgeniia <evgarnaga@gmail.com>
2023-06-02 22:12:48 +0600

    Добавляет адаптив (industry-solutions)

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,7 @@ import {ieFix} from './utils/ie-fix';
 import {initModals} from './modules/init-modals';
 import {showInputs} from './modules/show-inputs';
 import {featuresSlider} from './modules/features-slider';
+import {accordionIndustrySolutions} from './modules/accordion-industry-solutions';
 
 // Utils
 // ---------------------------------
@@ -15,3 +16,4 @@ ieFix();
 initModals();
 showInputs();
 featuresSlider();
+accordionIndustrySolutions();
