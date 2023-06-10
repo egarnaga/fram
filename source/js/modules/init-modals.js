4d7f3f54c97afe475b4ee70944414a9c28818144
Evgeniia <evgarnaga@gmail.com>
2023-06-11 01:17:25 +0600

    Добавляет modal--error

--- source/js/modules/init-modals.js
+++ source/js/modules/init-modals.js
@@ -5,6 +5,8 @@ const modalSuccess = document.querySelector('.modal--success');
 const modalSuccessBtns = document.querySelectorAll('[data-modal="success"]');
 const modalCall = document.querySelector('.modal--call');
 const modalCallBtns = document.querySelectorAll('[data-modal="call"]');
+const modalError = document.querySelector('.modal--error');
+const modalErrorBtns = document.querySelectorAll('[data-modal="error"]');
 
 // аргументы setupModal(modal, closeCallback, modalBtns, openCallback, noPrevDefault)
 // возможна инициализация только с первыми аргументом,
@@ -26,6 +28,9 @@ const initModals = () => {
   }
   if (modalCall && modalCallBtns.length) {
     setupModal(modalCall, false, modalCallBtns, false, false);
+  }
+  if (modalError && modalErrorBtns.length) {
+    setupModal(modalError, false, modalErrorBtns, false, false);
   }
 };
 
