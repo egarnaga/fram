86812db509eea17514ba5f27acfb9fd46e32df80
Evgeniia <evgarnaga@gmail.com>
2023-06-12 04:34:34 +0600

    правки

--- source/js/modules/header-light-scroll.js
+++ source/js/modules/header-light-scroll.js
@@ -1,19 +1,21 @@
 const headerLightScroll = () => {
 
   const headerScroll = document.querySelector('.header-scroll');
+  const key = document.getElementById('light-dark');
 
   function scrollFunction() {
-    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1 ) {
       headerScroll.classList.add('header--light');
     } else {
       headerScroll.classList.remove('header--light');
     }
   }
 
-  if (headerScroll) {
-    window.onscroll = function() {scrollFunction()};
+  if (key) {
+    if (headerScroll) {
+      window.onscroll = function() {scrollFunction()};
+    }
   }
-
 };
 
 export {headerLightScroll};
